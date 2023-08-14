688eedbc626fc509d13f4cb804fb60accbaa285c
Erwin Kuechel <ekuchel@student.42wolfsburg.de>
2023-08-14 17:16:51 +0200

    both motors work

--- Project03/bonus_03/bonus_03.ino
+++ Project03/bonus_03/bonus_03.ino
@@ -1,13 +1,13 @@
 #include <Servo.h>
 
-Servo leftright;  // first servo leftright
-Servo updown;  // second servo leftright
+Servo leftright;  // first motor
+Servo updown;  // second motor
 
 void setup() {
-  leftright.attach(4);
+  leftright.attach(4); //attach output for motors to pins 3 and 4
   updown.attach(3);
 
-  leftright.write(0);
+  leftright.write(0); //set motors in default position at 0 degrees
   updown.write(0);
 }
 
@@ -20,6 +20,7 @@ void loop() {
   // find the maximum light value and corresponding sensor
   int maxLightValue = max(lightValue1, max(lightValue2, lightValue3));
 
+  //motors continiously move until we can determine where it is
   if (lightValue1 < 100 && lightValue2 < 100 && lightValue3 < 100) {
     leftright.write(0);
     updown.write(0);
@@ -35,21 +36,23 @@ void loop() {
     delay(700);
   }
 
-  // if (maxLightValue >= 800) {
-  //   if (maxLightValue == lightValue1) {
-  //     // turn leftrights towards sensor 1 (right one on the breadboard)
-  //     leftright.write(0);   
-  //   }
-  //   else if (maxLightValue == lightValue2) {
-  //     // turn leftrights towards sensor 2 (middle one on the breadboard)
-  //     leftright.write(90);  
-  //   }
-  //   else {
-  //     // turn leftrights towards sensor 3 (left one on the breadboard)
-  //     leftright.write(180);
-  //   }
-  // }
+  //motor 1: works horisontally, shows which sensor is closest to light source
+  if (maxLightValue >= 800) {
+    if (maxLightValue == lightValue1) {
+      // turn leftrights towards sensor 1 (right one on the breadboard)
+      leftright.write(0);
+    }
+    else if (maxLightValue == lightValue2) {
+      // turn leftrights towards sensor 2 (middle one on the breadboard)
+      leftright.write(90);
+    }
+    else {
+      // turn leftrights towards sensor 3 (left one on the breadboard)
+      leftright.write(180);
+    }
+  }
 
+  //motor 2: works vertically, shows how far is the light source from the sensor
     if (maxLightValue < 800){
       updown.write(0);
     }
@@ -66,5 +69,5 @@ void loop() {
       updown.write(180);
     }
 
-    delay(700);  // delay before checking again
+    delay(700);  // delay before checking input again
   }
